b6269d017578585bf48effe401af4c5f81f213d8
BuildTools <unconfigured@null.spigotmc.org>
2022-11-21 00:12:34 +0800

    1121

--- js/navigation.js
+++ js/navigation.js
@@ -1,7 +1,5 @@
 function changemenu() {
-  //通过父元素li，找到兄弟元素ul
   var bList = this.parentNode.getElementsByTagName("ul")[0];
-  //CSS交替更换来实现显、隐
   if (bList.className == "myHide")
     bList.className = "myShow";
   else
@@ -9,13 +7,12 @@ function changemenu() {
 }
 window.onload = function () {
   var oList = document.getElementById("listUL");
-  var nList = oList.childNodes; //子元素
+  var nList = oList.childNodes; 
   var sList;
   for (var i = 0; i < nList.length; i++) {
-    //如果子元素为li，且这个li有子菜单ul
     if (nList[i].tagName == "LI" && nList[i].getElementsByTagName("ul").length) {
-      slist = nList[i].firstChild; //找到超链接
-      slist.onclick = changemenu; //动态添加点击函数
+      slist = nList[i].firstChild; 
+      slist.onclick = changemenu; 
     }
   }
 }
